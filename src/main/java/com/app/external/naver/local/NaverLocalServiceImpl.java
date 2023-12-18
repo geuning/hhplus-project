888680c3755ecc93bfef5e4c2f9b1246256d8362
junggeun <thethe133@naver.com>
2023-12-18 18:20:06 +0900

    api

--- src/main/java/com/app/external/naver/local/NaverLocalServiceImpl.java
+++ src/main/java/com/app/external/naver/local/NaverLocalServiceImpl.java
@@ -1,4 +1,36 @@
 package com.app.external.naver.local;
 
-public class NaverLocalServiceImpl {
+import com.app.external.place.PlaceSearchResponseDto;
+import com.app.external.place.PlaceSearchService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NaverLocalServiceImpl implements PlaceSearchService {
+
+    private final String naverClientId;
+    private final String naverSecretId;
+    private final NaverLocalClient naverLocalClient;
+
+    public NaverLocalServiceImpl(
+            @Value("${naver.client.id}") String naverClientId,
+            @Value("${naver.client.secret}") String naverSecretId, NaverLocalClient naverLocalClient){
+        this.naverClientId = naverClientId;
+        this.naverSecretId = naverSecretId;
+        this.naverLocalClient = naverLocalClient;
+    }
+
+    @Override
+    public List<PlaceSearchResponseDto> searchPlaceWithKeyword(String keyword) {
+        return null;
+    }
+
+
+
+
+
+
+
 }
